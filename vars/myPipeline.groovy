48761c779d311647e7fd428f501579a45e4bdf1b
Tadeu Mendonça <tadeu.mendonca@outlook.com>
2020-01-27 16:19:47 -0300

    My pipeline snippet.

---
+++ vars/myPipeline.groovy
@@ -0,0 +1,21 @@
+def call(Map pipelineParams) {
+
+    pipeline {
+        agent any
+        tools {nodejs "NodeJS"}
+        stages {
+            stage('Checkout Git') {
+                steps{
+                    gitCheckout()
+                }
+            }
+            stage('Build') {
+                steps{
+                    script{
+                        sh 'npm install'
+                    }
+                }
+            }
+        }
+    }
+}
