106325a8de7fd0f15f3599e2d06f28c3d750b541
Tadeu Mendonça <tadeu.mendonca@outlook.com>
2020-01-28 15:33:01 -0300

    Fix

--- vars/dockerRemoveImages.groovy
+++ vars/dockerRemoveImages.groovy
@@ -1,5 +1,5 @@
 def call(Map stageParams) { 
-    def imageList = sh label: 'Docker List Unused Images', returnStdout: true, script: "docker images --filter \"before=${stageParams.imageName}\" --format "{{.ID}}"  ${stageParams.appName}"
+    def imageList = sh label: 'Docker List Unused Images', returnStdout: true, script: "docker images --filter \"before=${stageParams.imageName}\" --format \"{{.ID}}\"  ${stageParams.appName}"
     
     for (image in imageList) {
         def statusCode = sh label: 'Remove Image', returnStatus: true, script: "docker image rm ${image}"
