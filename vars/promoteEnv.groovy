a0969b8270ac6267f4878ab430cef6919c40cc1d
Tadeu Mendonça <tadeu.mendonca@outlook.com>
2020-01-28 03:25:42 -0300

    Fix

--- vars/promoteEnv.groovy
+++ vars/promoteEnv.groovy
@@ -1,9 +1,8 @@
 def call(Map stageParams) { 
-    def statusCode = input message: "Promote to ${stageParams.environment}?", ok: 0
+    def statusCode = input message: "Promote to ${stageParams.environment}?", ok: 'true'
 
-    echo "${statusCode}"
     // Check result
-    if ( statusCode != 0 ){
+    if ( statusCode != 'true'){
         currentBuild.result = 'FAILURE'
         error('Failed to Promote Env')
     } else {
