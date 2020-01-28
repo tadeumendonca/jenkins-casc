799b40adffd787df4ccd2077a91b1f9ffaeca71b
Tadeu Mendonça <tadeu.mendonca@outlook.com>
2020-01-28 03:22:52 -0300

    Debug

--- vars/promoteEnv.groovy
+++ vars/promoteEnv.groovy
@@ -1,5 +1,7 @@
 def call(Map stageParams) { 
     def statusCode = input "Promote to ${stageParams.environment}?"
+
+    echo "${statusCode}"
     // Check result
     if ( statusCode != 0 ){
         currentBuild.result = 'FAILURE'
