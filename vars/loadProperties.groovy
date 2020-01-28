060875be7e38a31de79938d7cc7b07241e0647e7
Tadeu Mendonça <tadeu.mendonca@outlook.com>
2020-01-28 12:35:00 -0300

    Fix

--- vars/loadProperties.groovy
+++ vars/loadProperties.groovy
@@ -1,6 +1,8 @@
 def call(Map stageParams) { 
+    // Load Properties File
     def props = readProperties file: 'jenkins.properties'
-
+    
+    // Add each property to Environment Variables 
     for (entry in props) {
         env["${entry.key}"] = "${entry.value}"
     }
